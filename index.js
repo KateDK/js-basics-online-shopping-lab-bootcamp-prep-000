e3f1056d777358194b782eb312ccdb644de9e4e3
Kate <kate.dubitski@gmail.com>
2018-02-01 21:07:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ function placeOrder(cardNumber) {
   // write your code here
   if(cardNumber){
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
-    cart.splice(0,cart.length-1);
+    cart.splice(0,cart.length);
   }else{
     console.log("Sorry, we don't have a credit card on file for you.");
   }
